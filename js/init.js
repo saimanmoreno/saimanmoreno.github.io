d46017c18dfae927da954e8151a03e1358c867ae
Simão Moreno <simaojmoreno@gmail.com>
2023-01-27 18:23:09 +0000

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Edit the date here
+
+$(document).ready(function () {
+
+	$("#countdown").countdown({
+		date: "2 Mars 2024 12:00:00",
+		travel: "8 February 2021 02:25:00",
+		inpt: "1 July 2021 10:00:00",
+		format: "on"
+	});
+
+});	
